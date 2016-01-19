e9e6c34010516418a0d00763ee5b104682b9558c
Alex Ko <alexko@utexas.edu>
2016-01-18 20:14:20 -0700

    implemented BouncyDancer class

--- src/bouncyDancer.js
+++ src/bouncyDancer.js
@@ -1,6 +1,10 @@
 var BouncyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('bouncy');
+  this.y = top;
+  this.x = left;
+  this.dx = Math.floor((Math.random() * 5)) + 1;
+  this.dy = Math.floor((Math.random() * 5)) + 1;
 };
 
 BouncyDancer.prototype = Object.create(Dancer.prototype);
@@ -8,4 +12,19 @@ BouncyDancer.prototype.constructor = BouncyDancer;
 
 BouncyDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
+
+  this.move();
+};
+
+BouncyDancer.prototype.move = function() {
+  if(this.x < 0 || this.x > $('body').width()) {
+    this.dx = -this.dx; 
+  }
+  if(this.y < 0 || this.y > $('body').height()) {
+    this.dy = -this.dy;
+  }
+
+  this.x += this.dx;
+  this.y += this.dy;
+  this.setPosition(this.y, this.x);
 };
