5248367d4407e0c0d5f776494dc71b44d0fed6c8
Alex Ko <alexko@utexas.edu>
2016-01-19 16:30:11 -0700

    fixed bug where dx and dy were undefined when intial step is invoked

--- src/bouncyDancer.js
+++ src/bouncyDancer.js
@@ -1,8 +1,8 @@
 var BouncyDancer = function(top, left, timeBetweenSteps){
-  Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node.addClass('bouncy');
   this.dx = Math.floor((Math.random() * 5)) + 1;
   this.dy = Math.floor((Math.random() * 5)) + 1;
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('bouncy');
 };
 
 BouncyDancer.prototype = Object.create(Dancer.prototype);
@@ -15,17 +15,15 @@ BouncyDancer.prototype.step = function(){
 
 BouncyDancer.prototype.move = function() {
   if(this.isDancing) {
-    if(this.dx && this.dy){
-      if(this.x < 0 || this.x > $('body').width()) {
-        this.dx = -this.dx; 
-      }
-      if(this.y < 0 || this.y > $('body').height()) {
-        this.dy = -this.dy;
-      }
-
-      this.x += this.dx;
-      this.y += this.dy;
-      this.setPosition(this.y, this.x);
+    if(this.x < 0 || this.x > $('body').width()) {
+      this.dx = -this.dx; 
     }
+    if(this.y < 0 || this.y > $('body').height()) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+    this.setPosition(this.y, this.x);
   }  
 };
