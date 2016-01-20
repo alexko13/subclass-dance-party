5b06921a2b63aad5ff96927170f735851b6f5d26
Alex Ko <alexko@utexas.edu>
2016-01-19 20:26:15 -0700

    now bounces from other dancers when collisions occur

--- src/bouncyDancer.js
+++ src/bouncyDancer.js
@@ -15,15 +15,37 @@ BouncyDancer.prototype.step = function(){
 
 BouncyDancer.prototype.move = function() {
   if(this.isDancing) {
-    if(this.x < 0 || this.x > $('body').width()) {
-      this.dx = -this.dx; 
-    }
-    if(this.y < 0 || this.y > $('body').height()) {
-      this.dy = -this.dy;
-    }
-
+    this.bounceFromWall();
+    this.bounceFromDancer();
     this.x += this.dx;
     this.y += this.dy;
     this.setPosition(this.y, this.x);
-  }  
+  }
+};
+
+BouncyDancer.prototype.bounceFromWall = function() {
+  if(this.x < 0 || this.x > $('body').width()) {
+    this.dx = -this.dx;
+  }
+
+  if(this.y < 0 || this.y > $('body').height()) {
+    this.dy = -this.dy;
+  }
+};
+
+BouncyDancer.prototype.bounceFromDancer = function() {
+  var thisDancer = this;
+
+  window.dancers.forEach(function(otherDancer) {
+    if(thisDancer !== otherDancer) {
+      if((thisDancer.x >= otherDancer.x - 10 && thisDancer.x <= otherDancer.x + 10) && (thisDancer.y >= otherDancer.y - 10 && thisDancer.y <= otherDancer.y + 10)) {
+        thisDancer.dx = -thisDancer.dx;
+        thisDancer.dy = -thisDancer.dy;
+        if(otherDancer instanceof BouncyDancer) {
+          otherDancer.dx = -otherDancer.dx;
+          otherDancer.dy = -otherDancer.dy;
+        }
+      }
+    }
+  });
 };
